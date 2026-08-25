877ecef97a1030fdf3f96bf330b9464f7a1983ee
James Crockett <201571017+james-crockettt@users.noreply.github.com>
2026-08-25 14:35:11 +0930

    wip: Implement computer answer generation logic

--- csharp/rock-paper-scissors/RockPaperScissors/Program.cs
+++ csharp/rock-paper-scissors/RockPaperScissors/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Computer answer generation logic.
+
+// Generate number, 0 (rock), 1 (paper), or 2 (scissors).
+Random computer = new Random();
+Console.WriteLine(computer.Next(0, 3));
+
+// Map number to corresponding answer and assign variable
